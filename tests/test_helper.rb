90f7734233f2a37cdc101bcc347265714fd0f02e
Remco van 't Veer <remco@remworks.net>
2015-03-02 10:34:41 +0100

    Release 1.2.1

--- tests/test_helper.rb
+++ tests/test_helper.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 #
-# Copyright (c) 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014 - R.W. van 't Veer
+# Copyright (c) 2006-2015 - R.W. van 't Veer
 
 require 'stringio'
 require 'pp'
