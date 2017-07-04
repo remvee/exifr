b794582095aad11c81f984df07621c6492a097a0
Hoang Xuan Phu <phuhimself@phunehehe.net>
2017-07-04 21:39:09 +0800

    Remove circular require

--- lib/exifr.rb
+++ lib/exifr.rb
@@ -1,15 +1,4 @@
 # Copyright (c) 2006-2015 - R.W. van 't Veer
 
-require 'logger'
-
-module EXIFR
-  class MalformedImage < StandardError; end
-  class MalformedJPEG < MalformedImage; end
-  class MalformedTIFF < MalformedImage; end
-
-  class << self; attr_accessor :logger; end
-  self.logger = Logger.new(STDERR)
-end
-
 require 'exifr/jpeg'
 require 'exifr/tiff'
--- lib/exifr/tiff.rb
+++ lib/exifr/tiff.rb
@@ -1,6 +1,6 @@
 # Copyright (c) 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015 - R.W. van 't Veer
 
-require 'exifr'
+require 'exifr/common'
 require 'rational'
 require 'enumerator'
 
